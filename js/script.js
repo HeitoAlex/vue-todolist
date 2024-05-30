9615a2e5812a9b007c19f1b730fc86afbeb621d4
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-05-30 19:06:53 +0200

    non funziona come dovrebbe

--- js/script.js
+++ js/script.js
@@ -3,10 +3,29 @@ const { createApp } = Vue
     createApp({
         data() {
         return {
-        
+            toDoList:[
+                {
+                    text: 'Cavalcare un drago vestito da indiano',
+                    done: false
+                },{
+                    text: 'Conquistare Kepler-22b',
+                    done: false
+                },{
+                    text: 'Entrare nella ciurma di One Piece',
+                    done: false
+                },{
+                    text: 'Discutere di filosofia con Satana',
+                    done: false
+                }
+            ]
+
         }
     },
     methods: {
+        switchDone : function(itemIndex){
+            this.toDoList[itemIndex].done = !this.toDoList[itemIndex].done;
+        },
+
         
     }
     }).mount('#app')
